8e150372e72e0da19c47fb21c0813a59809088d2
ElvisDeSchelvis <A.Sterckx@student.tudelft.nl>
2025-03-26 18:16:57 +0100

    Area calculations
    Added calculations for fan area based on isentropic relations. Then used hub to tip ratio to calculate tip radius.

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+f = 80 #kg/s
 rho_ISA = 0.413 # kg/m^3
 T_ISA = 223.2 # K
 R = 287.05 # J/kgK
@@ -9,6 +10,7 @@ M_ax = 0.6
 gamma = 1.4
 beta_tt = 1.6
 eta_initial = 0.9
+hub_to_tip = 0.3
 
 P_t0 = P_ISA * (1 + ((gamma-1)/2)*M_inf**2) ** ((gamma-1)/gamma)
 T_t0 = T_ISA * (1 + ((gamma-1)/2)*M_inf**2)
@@ -19,3 +21,17 @@ T_t1 = T_ISA * (1 + ((gamma-1)/2)*M_ax**2)
 dT_t = (T_t1/eta_initial)*(beta_tt**((gamma-1)/gamma) - 1)
 
 print(dT_t*1005)
+
+
+### Area calculations
+rho_1 = rho_ISA * ( (1 + 0.5 * (gamma-1) * M_ax**2) / (1 + 0.5 * (gamma-1) * M_inf**2) )**(-1/(gamma-1))
+T_1 = T_ISA * ( (1 + 0.5 * (gamma-1) * M_ax**2) / (1 + 0.5 * (gamma-1) * M_inf**2) )**(-1)
+a_1 = np.sqrt(gamma * R * T_1)
+A_fan = f/(rho_1 * a_1 * M_ax)
+print(A_fan)
+print(rho_1)
+
+R_t = np.sqrt(A_fan/(np.pi * (1 -  hub_to_tip**2)))
+print(R_t)
+
+
