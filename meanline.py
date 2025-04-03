07a649f5a4fbf717ee504cbff84d975c51ad672b
Christos <margaritis.christos@outlook.com>
2025-04-03 11:54:18 +0200

    added stage properties estimation and stagnation properites estimation

--- meanline.py
+++ meanline.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy.typing import NDArray
+from ambiance import Atmosphere
 
 def getMeanLineRadius(rTip: float, hubToTip: float): #area average
     rHub: float =  rTip * hubToTip
@@ -58,6 +59,35 @@ def drawVelocityTriangles(alpha1: float, alpha2: float, beta1: float, beta2: flo
     plt.show()
 
 
+def getPropertiesAfterStage(Tin: float, Pin: float, rhoIn: float, C1mag: float, omega: float, rMean: float, alpha1: float, alpha2: float, cp: float, k: float = 1.4, etaIso: float = 1) -> tuple[float,float,float,float]:
+    U = omega * rMean #assuming constant radius
+    Ca = C1mag * np.cos(alpha1)
+    
+    DeltaTis = U * Ca / cp * (np.tan(alpha2) - np.tan(alpha1)) 
+    DeltaT = DeltaTis / etaIso
+    
+    T03is = Tin + DeltaTis
+    T03   = Tin + DeltaT
+    
+    P03 = (T03is/T03) ** (k / (k-1)) * Pin
+    rho03 = (T03is/T03) ** (1 / (k-1)) * rhoIn
+    
+    
+    return T03, P03, rho03, P03/Pin
+
+
+def getStagnationInletProperties(h: float, M: float, k: float = 1.4) -> tuple[float, float, float]:
+    atmoConditions = Atmosphere(h)
+    T = atmoConditions.temperature
+    P = atmoConditions.pressure
+    rho = atmoConditions.density
+    
+    T0 = (1 + (k-1)/2 * M**2) * T
+    P0 = (T0/T) ** (k/(k-1)) * P
+    rho0 = (T0/T) ** (1/(k-1)) * rho
+    
+    return T0, P0, rho0
+    
 
 if __name__ == "__main__":
     alpha1, alpha2, beta1, beta2 = computeVelocityTrianglesWithRKnown(0.3954,0.5633,0.5)
